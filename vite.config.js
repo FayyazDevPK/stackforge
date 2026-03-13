f07a2def17ee90bc5cbcacce0d9251f798ba1204
fayyazliaquat <fayyazliaquat70@gmail.com>
2026-03-14 00:32:56 +0500

    build: update vite config to resolve chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // This splits out all third-party dependencies into their own chunks
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
+})
