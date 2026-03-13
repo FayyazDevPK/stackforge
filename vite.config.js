9b4f674dc692bdef4597e5327a7ad2532a94271a
fayyazliaquat <fayyazliaquat70@gmail.com>
2026-03-14 00:40:17 +0500

    fix: resolve duplicate defineConfig errors

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // Split all third-party dependencies into their own chunks
           if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0]
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         }
       }
