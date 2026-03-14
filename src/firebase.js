fd13e7b65afd4d8708d8f0b32100597566c3430c
fayyazliaquat <fayyazliaquat70@gmail.com>
2026-03-15 02:55:55 +0500

    Fix: use correct authDomain env variable

--- src/firebase.js
+++ src/firebase.js
@@ -14,4 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider();
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const googleProvider = provider;
